7d717d1e4ad6fe7f1b3490a5ce898e85c5488065
kodati-nag-durga-rao <kndr476@gmail.com>
2024-09-16 07:09:45 +0530

    Updated weather_services.dart for weather data improvements

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,15 +1,16 @@
 
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/weather_model.dart';
 
 
 class WeatherApi {
-  static const String _apiKey = '7be22ced066fa43f72912e33c2364e7d'; // Replace with your actual API key
+  static const String _apiKey = '7be22ced066fa43f72912e33c2364e7d';
   static const String _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-  // Function to fetch weather data based on latitude and longitude
+  /// Function to fetch weather data based on latitude and longitude
   static Future<WeatherModel?> getWeather(double latitude, double longitude) async {
     final url = Uri.parse('$_baseUrl?lat=$latitude&lon=$longitude&units=metric&appid=$_apiKey');
 
@@ -17,15 +18,18 @@ class WeatherApi {
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
-        // Parse the JSON response into WeatherModel
         final data = json.decode(response.body);
         return WeatherModel.fromJson(data);
       } else {
-        print('Failed to load weather data: ${response.statusCode}');
+        if (kDebugMode) {
+          print('Failed to load weather data: ${response.statusCode}');
+        }
         return null;
       }
     } catch (e) {
-      print('Error fetching weather data: $e');
+      if (kDebugMode) {
+        print('Error fetching weather data: $e');
+      }
       return null;
     }
   }
